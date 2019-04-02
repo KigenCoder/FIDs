82845459d2b3109e506437979746ca8ac1a080f7
kigenCoder <JImmiBraun@gmail.com>
2019-04-02 12:16:41 +0300

    Fixed Civil security duplicate insertions

--- app/Http/Controllers/AppAuthController.php
+++ app/Http/Controllers/AppAuthController.php
@@ -9,7 +9,8 @@ use App\User;
 use Illuminate\Support\Facades\DB;
 use App\MarketData;
 
-class AppAuthController extends Controller{
+class AppAuthController extends Controller
+{
 
     public function signup(Request $request)
     {
@@ -22,14 +23,14 @@ class AppAuthController extends Controller{
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
-            'user_role_id'=> 3 //Defaults to enumerator 
+            'user_role_id' => 3 //Defaults to enumerator
         ]);
         $user->save();
         return response()->json([
             'message' => 'Successfully created user!'
         ], 201);
     }
-  
+
     public function login(Request $request)
     {
         $request->validate([
@@ -39,7 +40,7 @@ class AppAuthController extends Controller{
         ]);
 
         $credentials = request(['email', 'password']);
-        if(!Auth::attempt($credentials))
+        if (!Auth::attempt($credentials))
             return response()->json([
                 'message' => 'Unauthorized'
             ], 401);
@@ -48,9 +49,9 @@ class AppAuthController extends Controller{
         //dd($user->id);
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
-        
+
         //Remember token
-        if($request->remember_me){
+        if ($request->remember_me) {
             $token->expires_at = Carbon::now()->addWeeks(1);
         }
 
@@ -59,68 +60,88 @@ class AppAuthController extends Controller{
         return response()->json([
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString(), 
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString(),
             'user_data' => $this->appUserMarkets($user)
         ]);
     }
 
-    public function appUserMarkets(User $user){
+    public function appUserMarkets(User $user)
+    {
         $query = "SELECT u.id, u.name, um.market_id, m.market_name, m.system_id as market_type_id ";
         $query .= "from users u ";
         $query .= "JOIN user_markets um ON um.user_id = u.id ";
         $query .= "JOIN markets m ON m.id = um.market_id ";
         $query .= "WHERE u.id = $user->id";
-       return DB::select(DB::raw($query));
-          
+        return DB::select(DB::raw($query));
+
     }
 
 
-    public function uploadMarketData(Request $request){
-       $input = $request->all();
-       $metaData = $input["metaData"];
-       $marketData = $input["marketData"];
-       $year_name = $metaData["yearName"];
-       $month_id = $metaData["month_id"];
-       $week = $metaData["week"];
-       $market_id = $metaData['marketId'];
+    public function uploadMarketData(Request $request)
+    {
+        $input = $request->all();
+        $metaData = $input["metaData"];
+        $marketData = $input["marketData"];
+        $year_name = $metaData["yearName"];
+        $month_id = $metaData["month_id"];
+        $week = $metaData["week"];
+        $market_id = $metaData['marketId'];
 
-       $numberOfRecords = 0;
-       foreach($marketData as $indicator_id => $price){
-           $data = array();
-           $data["year_name"] = $year_name;
-           $data["month_id"] = $month_id;
-           $data["week"] = $week;
-           $data["market_id"] = $market_id;
-           $data["indicator_id"] = $indicator_id;
-           $data["price"] = $price;
+        $numberOfRecords = 0;
+        foreach ($marketData as $indicator_id => $price) {
+            $data = array();
+            $data["year_name"] = $year_name;
+            $data["month_id"] = $month_id;
+            $data["week"] = $week;
+            $data["market_id"] = $market_id;
+            $data["indicator_id"] = $indicator_id;
+            $data["price"] = $price;
 
-           $existingRecord = MarketData::where("year_name", $year_name)
-           ->where("month_id", $month_id)
-           ->where("week", $week)
-           ->where("market_id", $market_id)
-           ->where("indicator_id", $indicator_id)
-           ->first();
+            $civilSecurityID = "53";
+            $existingRecord = NULL;
 
-           if(is_null($existingRecord)){
-                 //Record does not exist
-                 MarketData::firstOrCreate($data);
-                 $numberOfRecords ++;
-           }  
-       }
+            if (strcasecmp($indicator_id, $civilSecurityID) == 0) {
+                //CIVIL INSECURITY - Monthly data
+                $existingRecord = MarketData::where("year_name", $year_name)
+                    ->where("month_id", $month_id)
+                    ->where("market_id", $market_id)
+                    ->where("indicator_id", $civilSecurityID)
+                    ->first();
+            } else {
+                //NON CIVIL INSECURITY - weekly data
+                $existingRecord = MarketData::where("year_name", $year_name)
+                    ->where("month_id", $month_id)
+                    ->where("week", $week)
+                    ->where("market_id", $market_id)
+                    ->where("indicator_id", $indicator_id)
+                    ->first();
+            }
 
-       $message = "";
-  
-       if($numberOfRecords > 0){
-           $message = "Successfully updated market data!";
-       }else{
-        $message = "No new records added!";
-       }
 
-       return response()->json([
-        'message' => $message,
-        'numberOfRecords' => $numberOfRecords
-    ], 
-    201);
+            if (is_null($existingRecord)) {
+                //Record does not exist
+                MarketData::firstOrCreate($data);
+                $numberOfRecords++;
+            }
+        }
+        
+        $message = NULL;
+        if ($numberOfRecords > 0) {
+            $message = "Successfully updated market data!";
+        } else {
+            $message = "No new records added!";
+        }
+
+        return response()->json([
+            'message' => $message,
+            'numberOfRecords' => $numberOfRecords
+        ],
+            201);
+
+    }
+
+    protected function civilSecurity()
+    {
 
     }
 
@@ -129,10 +150,11 @@ class AppAuthController extends Controller{
      *
      * @return [json] user object
      */
-    public function user(Request $request){
+    public function user(Request $request)
+    {
         return response()->json($request->user());
     }
-  
+
     /**
      * Logout user (Revoke the token)
      *
@@ -145,7 +167,6 @@ class AppAuthController extends Controller{
             'message' => 'Successfully logged out'
         ]);
     }
-  
 
 
 }
